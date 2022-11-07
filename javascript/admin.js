6525307f93b0b50f7332b8ba9acb3c091c8d44f7
George Ciesinski <george.ciesin@gmail.com>
2022-11-07 18:03:42 -0500

    Added favourite button functionality

--- javascript/admin.js
+++ javascript/admin.js
@@ -1,7 +1,17 @@
-// News
+// Page
+const toggleFavourite = function(event) {
+    // Get Button
+    const buttonFavourite = this;
+    if (buttonFavourite.innerText === "star_border") {
+        buttonFavourite.innerText = "star";
+    } else if (buttonFavourite.innerText === "star") {
+        buttonFavourite.innerText = "star_border";
+    }
+}
 
+// News
 const hideNews = function(event) {
-    // Get Target Span
+    // Get Button
     const newsSpan = this;
     // Get the p element of button's news-item
     const newsItem = event.target.parentNode.parentNode;
@@ -21,4 +31,8 @@ const hideNews = function(event) {
 
 // Get elements
 const newsButtons = document.querySelectorAll(".news-button");
+const favouriteButtons = document.querySelectorAll(".favourite");
+
+// Set Event Listeners
 newsButtons.forEach(newsButton => newsButton.addEventListener("click", hideNews));
+favouriteButtons.forEach(favouriteButton => favouriteButton.addEventListener("click", toggleFavourite));
