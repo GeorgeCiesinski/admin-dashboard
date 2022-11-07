f4cd151e847f378186b3ea9d094eeb9d08bcc5c1
George Ciesinski <george.ciesin@gmail.com>
2022-11-07 18:15:13 -0500

    Delete page functionality added

--- javascript/admin.js
+++ javascript/admin.js
@@ -9,30 +9,43 @@ const toggleFavourite = function(event) {
     }
 }
 
+const deletePage = function(event) {
+    const buttonDelete = this;
+    const pageElement = buttonDelete.parentNode.parentNode;
+    const pageTitle = pageElement.getElementsByTagName("h4")[0].innerText;
+    let confirmDelete = confirm("Are you sure you want to delete this page?");
+    if (confirmDelete) {
+        pageElement.remove();
+        alert(`\"${pageTitle}\" successfully deleted.`)
+    }
+}
+
 // News
 const hideNews = function(event) {
     // Get Button
-    const newsSpan = this;
+    const buttonHide = this;
     // Get the p element of button's news-item
-    const newsItem = event.target.parentNode.parentNode;
+    const newsItem = buttonHide.parentNode.parentNode;
     const newsP = newsItem.getElementsByTagName("p");
     // Toggle visibility
-    if (newsSpan.innerText === "visibility_off") {
+    if (buttonHide.innerText === "visibility_off") {
         // Hide P
         newsP[0].hidden = true;
         // Change span icon
-        newsSpan.innerText = "visibility";
-    } else if (newsSpan.innerText === "visibility") {
+        buttonHide.innerText = "visibility";
+    } else if (buttonHide.innerText === "visibility") {
         // Unhide P
         newsP[0].hidden = false;
-        newsSpan.innerText = "visibility_off";
+        buttonHide.innerText = "visibility_off";
     }
 }
 
 // Get elements
 const newsButtons = document.querySelectorAll(".news-button");
 const favouriteButtons = document.querySelectorAll(".favourite");
+const deleteButtons = document.querySelectorAll(".delete");
 
 // Set Event Listeners
 newsButtons.forEach(newsButton => newsButton.addEventListener("click", hideNews));
 favouriteButtons.forEach(favouriteButton => favouriteButton.addEventListener("click", toggleFavourite));
+deleteButtons.forEach(deleteButton => deleteButton.addEventListener("click", deletePage));
